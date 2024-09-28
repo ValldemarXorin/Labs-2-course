9b85ea17a50db318b3612c204320cc8196ee1c54
ValldemarXorin <vova3089927@gmail.com>
2024-09-28 17:23:24 +0300

    Change gitignore

---
+++ Lab1 3sem/src/main.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include "Film.h"
+#include "DatabaseFilms.h"
+#include "MyList.h"
+#include "locale.h"
+
+using std::cout;
+using std::cin;
+using std::endl;
+using std::string;
+
+
+int main() {
+	bool exit = true;
+	int choose;
+	DatabaseFilms* my_films = new DatabaseFilms;
+	while (exit) {
+		cout << "Choose function:\n1. Add film\n2. Check films list\n3. Update film\n4. Delete film" << endl;
+		cin >> choose;
+		switch (choose) {
+		case 1:
+			my_films->create_film();
+			break;
+		case 2:
+			my_films->print_database();
+			break;
+		case 3:
+			my_films->update_film();
+			break;
+		case 4:
+			my_films->delete_film();	
+			break;
+		}
+		cout << "Do you want continue? If yes, enter 1, else 0" << endl;
+		cin >> exit;
+	}
+	delete my_films;
+	return 0;
+}
+
+//ссылки посмотреть глубже и добавить

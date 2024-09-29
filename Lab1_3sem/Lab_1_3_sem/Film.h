7ea606c4c9511c0cab342ab4f426caece777279e
ValldemarXorin <vova3089927@gmail.com>
2024-09-29 17:21:38 +0300

    q

---
+++ Lab1_3sem/Lab_1_3_sem/Film.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <iostream>
+#include <string>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::string;
+
+class Film {
+private:
+	string name;
+
+public:
+	Film(const string& enter_name) : name(enter_name) {}
+
+	void set_name(const string& new_name) {
+		name = new_name;
+	}
+
+	string get_name() {
+		return name;
+	}
+};
